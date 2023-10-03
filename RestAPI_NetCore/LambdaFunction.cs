e2e514b2af3407381c1af0870b61bb9ee6ded665
ChakriyaC <ChakriyaC@CHAKRIYAC-NB>
2023-10-03 22:23:55 +0700

    Add project files.

---
+++ RestAPI_NetCore/LambdaFunction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace RestAPI_NetCore
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<StartupBase>()
+                .UseLambdaServer();
+        }
+    }
+
+
+}
